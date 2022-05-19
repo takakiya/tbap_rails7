5ff4d5dcf37d30f5071d8ce32320d8eaa7c35b41
Takayuki.Y <takakiya@sea.plala.or.jp>
2022-05-19 07:22:50 +0000

    エラー確認フラグ追加（環境変数）

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/hello", to: "hello#index"
+  get "/error_check", to: "error_check#index"
 end
